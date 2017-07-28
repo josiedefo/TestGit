14ab56ea2b8ac13192fd51c7c7b8aba57436895c
josiedefo <jgseumo@gmail.com>
2017-07-27 22:51:20 -0600

    copied from GEA AMDS

---
+++ DocatoRepositoryImplFriend.java
@@ -0,0 +1,24 @@
+package com.docato.repository.impl;
+
+import com.xhive.dom.interfaces.XhiveLibraryChildIf;
+
+
+/**
+ * Hack: By having this code in the com.docato.repository.impl package,
+ *       we can get access to protected methods.
+ */
+public class DocatoRepositoryImplFriend {
+
+  private DocatoRepositoryImplFriend() { }
+  
+  
+  public static XhiveLibraryChildIf getLibraryChild(XhiveVersionableResource resource) {
+    return resource.getLibraryChild();
+  }
+  
+  public static XhiveLibraryChildIf getWorkspaceRepresentation(XhiveVersionableResource resource) {
+    return resource.getWorkSpaceRepresentation(null);
+  }
+  
+  
+}
